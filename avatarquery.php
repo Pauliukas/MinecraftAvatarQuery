7a49bea67cd91a993de29b11291cf3ff20e016a7
McLive <mail@mclive.eu>
2014-07-17 13:10:26 +0200

    Update avatarquery.php

--- avatarquery.php
+++ avatarquery.php
@@ -7,7 +7,8 @@
 //The following script is tested only with servers running on Minecraft 1.7.
 
 $SERVER_IP = "sky.freecraft.eu"; //Insert the IP of the server you want to query. Query must be enabled in your server.properties file!
-$SERVER_PORT = "25565"; //Insert the PORT of the server you want to query. Query must be enabled in your server.properties file!
+$SERVER_PORT = "25565"; //Insert the PORT of the server you want to ping. Needed to get the favicon, motd, players online and players max. etc
+$QUERY_PORT = "25565"; //Port of query.port="" in your server.properties. Needed for the playerlist! Can be the same like the port or different
 
 $HEADS = "3D"; //"normal" / "3D"
 $SHOW_FAVICON = "on"; //"off" / "on"
@@ -23,25 +24,8 @@ $TITLE_BLOCK_TWO = "Players";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Get Data and Status API Checker
-function get_data($url)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    $data       = curl_exec($ch);
-    $httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return array(
-        'status' => $httpStatus,
-        'data' => $data
-    );
-} 
-
-//Query the data from the server using Minecraft API (also known as IamPhoenix's API)
-$serverdata = get_data("http://mcapi.sweetcode.de/api/v2/?ip=" . $SERVER_IP . "&port=". $SERVER_PORT ."");
-$userlistserver = get_data("http://mcapi.sweetcode.de/api/v1/query/?ip=" . $SERVER_IP . ":" . $SERVER_PORT ."");
+$ping = json_decode(file_get_contents('http://api.minetools.eu/ping/' . $SERVER_IP . '/' . $SERVER_PORT . ''), true);
+$query = json_decode(file_get_contents('http://api.minetools.eu/query/' . $SERVER_IP . '/' . $QUERY_PORT . ''), true);
 
 //* DEBUG AREA
 //var_dump($serverdata);
@@ -50,11 +34,18 @@ $userlistserver = get_data("http://mcapi.sweetcode.de/api/v1/query/?ip=" . $SERV
 //echo "<br>";echo "<br>";
 //* DEBUG AREA
 
-// Json Decode
-$data_list     = json_decode($userlistserver["data"], true);
-$data_general  = json_decode($serverdata["data"], true);
-
 //Put the collected player information into an array for later use.
+if(empty($ping['error'])) { 
+	$version = $ping['version']['name'];
+	$online = $ping['players']['online'];
+	$max = $ping['players']['max'];
+	$motd = $ping['description'];
+	$favicon = $ping['favicon'];
+}
+
+if(empty($query['error'])) {
+	$playerlist = $query['Playerlist'];
+}
 $array_list = $data_list[$SERVER_IP]['player']['list'];
 
 $queryerror = "false";
@@ -66,7 +57,6 @@ $haserror = "false";
 if($data_general['status'] != "true") {
 	$haserror = "true";
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -103,34 +93,30 @@ if($data_general['status'] != "true") {
 							<td><b>IP</b></td>
 							<td><?php echo $SERVER_IP; ?></td>
 						</tr>
-					<?php if($haserror == "false") { ?>
+					<?php if(empty($ping['error'])) { ?>
 						<tr>
 							<td><b>Version</b></td>
-							<td><?php echo $data_general['version']; ?></td>
+							<td><?php echo $version; ?></td>
 						</tr>
 					<?php } ?>
-					<?php if($haserror == "false") { ?>
+					<?php if(empty($ping['error'])) { ?>
 						<tr>
 							<td><b>Players</b></td>
-							<td><?php echo "".$data_general['player']['currently']." / ".$data_general['player']['max']."";?></td>
+							<td><?php echo "".$online." / ".$max."";?></td>
 						</tr>
 					<?php } ?>
 						<tr>
 							<td><b>Status</b></td>
-							<td><? if($haserror == "false") { echo "<i class=\"icon-ok-sign\"></i> Server is online"; } else { echo "<i class=\"icon-remove-sign\"></i> Server is offline";}?></td>
+							<td><?php if(empty($ping['error'])) { echo "<i class=\"icon-ok-sign\"></i> Server is online"; } else { echo "<i class=\"icon-remove-sign\"></i> Server is offline";}?></td>
 						</tr>
-					<?php if($haserror == "false") { ?>
-						<tr>
-							<td><b>Latency</b></td>
-							<td><?php echo "".$data_general['list']['ping']."ms"; ?></td>
-						</tr>
-					<?php } ?>
-					<?php if($haserror == "false") { ?>
+					<?php if(empty($ping['error'])) { ?>
+					<?php if(!empty($favicon)) { ?>
 					<?php if ($SHOW_FAVICON == "on") { ?>
 						<tr>
 							<td><b>Favicon</b></td>
 							<td><img src='http://mcapi.sweetcode.de/api/v2/?favicon&ip=<?php echo $SERVER_IP;?>&port=<?php echo $SERVER_PORT;?>' width="64px" height="64px" style="float:left;"/></td>
 						</tr>
+					<?php } ?>
 					<?php } ?>
 					<?php } ?>
 					</tbody>
@@ -144,24 +130,19 @@ if($data_general['status'] != "true") {
 				} else {
 					$url = "https://cravatar.eu/helmavatar/";
 				}
-				if($queryerror == "false" && $haserror == "false") {
-				//Take the username values from the array & grab the avatars from Minotar.				
-				foreach($array_list as $key => $value) {
-					$users .= "<a data-placement=\"top\" rel=\"tooltip\" style=\"display: inline-block;\" title=\"".$value."\">
-								<img src=\"".$url.$value."/50\" size=\"40\" width=\"40\" height=\"40\" style=\"width: 40px; height: 40px; margin-bottom: 5px; margin-right: 5px; border-radius: 3px;\"/></a>";
-				}
-				//Display the avatars only when there are players online.
-				if($data_general['player']['currently'] > 0) {
-						print_r($users);
+
+				if(empty($query['error'])) {
+					if($playerlist != "null") { //is at least one player online? Then display it!
+						foreach ($playerlist as $player) { ?>
+							<a data-placement="top" rel="tooltip" style="display: inline-block;" title="<?php echo $player;?>">
+							<img src="<?php echo $url.$player;?>/50" size="40" width="40" height="40" style="width: 40px; height: 40px; margin-bottom: 5px; margin-right: 5px; border-radius: 3px; "/></a>
+				<?php	}
+					} else {
+						echo "<div class=\"alert\"> There are no players online at the moment!</div>";
 					}
-				//If no avatars can be shown, display an error.
-				else { 
-					echo "<div class=\"alert\"> There are no players online at the moment!</div>";
-				}
 				} else {
 					echo "<div class=\"alert\"> Query must be enabled in your server.properties file!</div>";
-				}				
-				?>
+				} ?>
 			</div>
 		</div>
 	</div>
